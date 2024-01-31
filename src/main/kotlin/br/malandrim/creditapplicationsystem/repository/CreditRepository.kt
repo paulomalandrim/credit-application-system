4bdd3454b467a3c5fa19212df060190d848676e2
paulomalandrim <paulo.malandrim@gmail.com>
2024-01-30 21:39:09 -0300

    .

--- src/main/kotlin/br/malandrim/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/kotlin/br/malandrim/creditapplicationsystem/repository/CreditRepository.kt
@@ -2,8 +2,15 @@ package br.malandrim.creditapplicationsystem.repository
 
 import br.malandrim.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+
+    @Query(value = "SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(creditCode: UUID): Credit?
 }
