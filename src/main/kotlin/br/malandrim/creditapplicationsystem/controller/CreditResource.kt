a604339001e2bc5e928fef68a5d38bc1f1986d51
paulomalandrim <paulo.malandrim@gmail.com>
2024-02-02 16:40:52 -0300

    criação do findByCustomerId

--- src/main/kotlin/br/malandrim/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/br/malandrim/creditapplicationsystem/controller/CreditResource.kt
@@ -1,13 +1,20 @@
 package br.malandrim.creditapplicationsystem.controller
 
 import br.malandrim.creditapplicationsystem.dto.CreditDto
+import br.malandrim.creditapplicationsystem.dto.CreditViewList
+import br.malandrim.creditapplicationsystem.entity.Credit
+import br.malandrim.creditapplicationsystem.entity.Customer
 import br.malandrim.creditapplicationsystem.repository.CreditRepository
 import br.malandrim.creditapplicationsystem.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.Mapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collector
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/v1/credit")
@@ -20,9 +27,24 @@ class CreditResource(
         val credit = creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
     }
-//    fun findCreditById(){
-//
-//    }
+
+    @GetMapping
+    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long):
+            List<CreditViewList> {
+
+       // Solução dada no curso
+//        return creditService.findAllByCustomer(customerId).stream().map {
+//            credit: Credit -> CreditViewList(credit)
+//        }.collect(Collectors.toList())
+
+        // Minha Solução
+        val credits = creditService.findAllByCustomer(customerId)
+        val mutableCreditViewList: MutableList<CreditViewList> = mutableListOf()
+        for (c in credits){
+                mutableCreditViewList.add(CreditViewList(c))
+        }
+        return mutableCreditViewList.toList()
+    }
 
 
 }
