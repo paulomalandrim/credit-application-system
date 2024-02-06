0317e48749be6bcd781aa0b6d79078d01d02acce
paulomalandrim <paulo.malandrim@gmail.com>
2024-02-06 17:18:47 -0300

    Configuração de Teste

---
+++ src/test/kotlin/br/malandrim/creditapplicationsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,11 @@
+package br.malandrim.creditapplicationsystem.service
+
+import io.mockk.MockKException
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockKException::class)
+class CustomerServiceTest {
+
+}
