ce92a5b37a6e5f2e5a18f1c6dda789f71d0ab043
paulomalandrim <paulo.malandrim@gmail.com>
2024-02-06 08:18:24 -0300

    fechamento das classes Services.

--- src/main/kotlin/br/malandrim/creditapplicationsystem/dto/CreditDto.kt
+++ src/main/kotlin/br/malandrim/creditapplicationsystem/dto/CreditDto.kt
@@ -4,7 +4,6 @@ import br.malandrim.creditapplicationsystem.entity.Credit
 import br.malandrim.creditapplicationsystem.entity.Customer
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Max
-import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
